2cc41e441d7f3d21a542fee2e0f0e53265cfc2ba
Vijay Vasanthapuram <vijay.vasanthapuram@arris.com>
2020-08-16 20:39:24 -0400

    Add Signout button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  signOut(): void {
+    console.log('sign out');
+    this.router.navigate(['/']);
   }
 
 }
